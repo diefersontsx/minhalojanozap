e2372faef133ae80d91fa955827c68bdc9227747
diefersontsx <appsigmadev@gmail.com>
2024-03-14 23:10:12 -0700

    feat: checkout step-finished

--- src/components/checkout/step-finish.tsx
+++ src/components/checkout/step-finish.tsx
@@ -1,14 +1,22 @@
-import { CheckoutSteps } from "@/types/checkout-steps";
-import { Dispatch, SetStateAction } from "react";
+import { useCheckoutStore } from "@/stores/checkout-store";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-type Props = {
-  setStep: Dispatch<SetStateAction<CheckoutSteps>>;
-};
 
-export default function StepFinish({ setStep }: Props) {
+export default function StepFinish() {
+
+  const { name } = useCheckoutStore(state => state)
+  const message = 'Oi, tudo bem?'
+  const linkWhatsapp = `https://wa.me//${process.env.NEXT_PUBLIC_WHATSAPP}?text${encodeURI(message)}`
   return (
 
-    <div></div>
+    <div className="text-center flex flex-col gap-5">
+      <p>Perfeito <strong>{name}</strong>!</p>
+      <p>Agora envie o seu pedido ao nosso whatsapp para concluir. Nosso atendente irá te guiar sobre o andamento do seu pedido</p>
+      <Button>
+        <Link target="_blank" href={linkWhatsapp}>Enviar pedido para o Whatsapp</Link>
+      </Button>
+    </div>
 
   )
 }
